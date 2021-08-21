2deea68461ae55653eb2e122b30542a780e99bb1
adeniau <adeniau@afoneinfrastructure.com>
2021-08-21 17:23:39 +0200

    commands

--- muddy-server.py
+++ muddy-server.py
@@ -15,7 +15,7 @@ from docopt import docopt
 import asyncio
 import websockets
 
-from core.utils import read_command
+from core.utils import read_command, prn
 from core.persist import migrate
 
 from service.room import room_service
@@ -40,6 +40,33 @@ async def main(ws, path):
 
         data = await read_command(ws)
 
+        # object perms: inspect/modify
+        # room perms:   visit/modify
+        # script perms: inspect/modify
+        # owners: UGO
+
+        if data["type"] == "say":     # Say something in public
+            await prn(ws, user.name + " just said: " + str(data["content"]))
+
+        # TO IMPLEMENT
+        if data["type"] == "ban":    # Ban a user
+            pass
+        if data["type"] == "priv":    # Change user privilege
+            pass
+        if data["type"] == "deletegrp": # Delete a group
+            pass
+        if data["type"] == "listgrp": # List groups
+            pass
+        if data["type"] == "creategrp": # Create a group
+            pass
+        if data["type"] == "grprm":   # Remove user from group
+            pass
+        if data["type"] == "grpadd":  # Add user to group
+            pass
+        if data["type"] == "chown":   # Change owner of an object/room/script
+            pass
+        if data["type"] == "chmod":   # Change perms of an object/room/script
+            pass
         if data["type"] == "dig":     # Create a room
             pass
         if data["type"] == "link":    # Link a room to another one
@@ -50,19 +77,17 @@ async def main(ws, path):
             pass
         if data["type"] == "give":    # Give something to someone
             pass
-        if data["type"] == "say":     # Say something in public
-            pass
         if data["type"] == "whisper": # Say something in private
             pass
         if data["type"] == "move":    # Move player to another room
             pass
-        if data["type"] == "copy":    # copy script
+        if data["type"] == "copy":    # Copy script
             pass
-        if data["type"] == "detach":  # detach script from obj
+        if data["type"] == "detach":  # detach script from obj/room/exit
             pass
-        if data["type"] == "attach":  # attach script to obj
+        if data["type"] == "attach":  # attach script to obj/room/exit
             pass
-        if data["type"] == "upload":  # create script
+        if data["type"] == "upload":  # Create or update a script
             pass
         if data["type"] == "save":    # Save my character
             pass
